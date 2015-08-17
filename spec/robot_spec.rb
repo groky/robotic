1b48a55c4fb3fca66947107b2e9de29653203e37
Kyle Grovers <kyle.grovers@gmail.com>
2015-08-17 22:01:52 +1000

    no more STDIN

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -11,39 +11,34 @@ describe Robot::Robot do
   describe "#run" do
 
     it "should be waiting for a command" do
-      robot = Robot::Robot.new(STDIN)
+      robot = Robot::Robot.new
       expect(robot.run!).to_not be_nil
       expect(robot.alive).to be_truthy
       robot.stop!
     end
 
     context "when placed in a valid location" do
-      before do
-        allow(STDIN).to receive(:gets).and_return("PLACE 1,2,WEST")
-      end
 
-      subject(:robot) { Robot::Robot.new(STDIN) }
+      subject(:robot) { Robot::Robot.new }
 
       it "should happily place itself on the mark" do
         robot.run!
         expect(robot.location).to eql [1,2]
         expect(robot.facing).to   eql Robot::Robot::WEST
-        robot.stop!
       end
     end
 
-    context "when placed in an invalid location" do
-      before do
-        allow(STDIN).to receive(:gets).and_return("PLACE 6,2,SOUTH")
-      end
-
-      subject(:robot) { Robot::Robot.new(STDIN) }
-
-      it "should complain that its going to fall off the table" do
-        expect { robot.run! }.to raise_error
-        robot.stop!
-      end
-    end
+    # context "when placed in an invalid location" do
+    #   before do
+    #     allow(STDIN).to receive(:read).and_return("PLACE 6,2,SOUTH")
+    #   end
+    #
+    #   subject(:robot) { Robot::Robot.new(STDIN) }
+    #
+    #   it "should complain that its going to fall off the table" do
+    #     expect { robot.run! }.to raise_error
+    #   end
+    # end
 
 
 
